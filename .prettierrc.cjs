dbea467d86d8bc93cdec36bad8e54e8da85b2002
hujiacheng <hujiacheng@iipcloud.onaliyun.com>
2025-11-10 10:50:43 +0800

    chore: add .editorconfig and update .prettierrc.cjs for consistent formatting

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,14 +5,14 @@ module.exports = {
   printWidth: 100, // 每行最大长度
   tabWidth: 2, // 缩进空格数
   useTabs: false, // 使用空格而不是 Tab
-  trailingComma: "es5", // 尾随逗号
+  trailingComma: 'es5', // 尾随逗号
   bracketSpacing: true, // 对象括号间距
-  arrowParens: "always", // 箭头函数参数括号
-  endOfLine: "lf", // 换行符（Unix）
+  arrowParens: 'always', // 箭头函数参数括号
+  endOfLine: 'lf', // 换行符（Unix）
 
   // Vue 特定
   vueIndentScriptAndStyle: false,
 
   // HTML
-  htmlWhitespaceSensitivity: "ignore",
-};
+  htmlWhitespaceSensitivity: 'ignore',
+}
